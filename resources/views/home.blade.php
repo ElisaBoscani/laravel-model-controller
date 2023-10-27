0ac7b9de6bfa3db813f2a6c8a4f4b2a81d87ef31
Elisa Boscani <elisabosc90@gmail.com>
2023-10-27 15:30:09 +0200

    messo in pagina tutti i valori della tabella

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,14 @@
 @section('content')
 
 <div class=" container ">
-  <div class="row row-cols-1 row-cols-md-2 g-3">
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5 g-3">
     @foreach($movies as $movie)
-    <div class="col">
-      <h3>{{$movie->title}}</h3>
+    <div class="col d-flex flex-column">
+      <h4>{{$movie->title}}</h4>
+      <span>{{$movie->original_title}}</span>
+      <span>{{$movie->nationality}}</span>
+      <span>{{$movie->date}}</span>
+      <span>{{$movie->vote}}</span>
     </div>
     @endforeach
   </div>
